cd7af0f058dcdade7b83f1082c4623f4328210b9
Łukasz Wołodkiewicz <l.wolodkiewicz@systemycallcenter.pl>
2022-03-08 11:33:02 +0000

    gitpod

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const Dialer = require('dialer').Dialer;
+const cors = require('cors');
+let bridge = null;
+const config = {
+   url: 'https://uni-call.fcc-online.pl',
+   login: 'focus16',
+   password: 'njhipjwr4kf'
+};
+
+Dialer.configure(config);  
+
+app.listen(3000, () => {
+   console.log('app listening on port 3000');
+});
+app.use(cors());
+app.get('/call/:number1/:number2', async (req, res) => {
+   const number1 = req.params.number1;
+   const number2 = req.params.number2;
+  
+   bridge = await Dialer.call(number1, number2);
+   res.json({success: true});
+  })
+  
+  app.get('/status', async (req, res) => {
+     let status = 'NONE';
+   if (bridge !== null) {
+     status = await bridge.getStatus();
+   }
+    res.json({success: true, status: status});
+  });
+  
+  app.post('/call/', (req, res) => {
+   const body = req.body;
+   console.log('body',body);
+   // bridge = Dialer.call(number1, number2);
+   res.json({ success: true });
+  })
+  
--- index.js
+++ index.js
@@ -9,24 +9,26 @@ app.get('/', (req, res) => {
    res.json({success: true});
 })
 
-function getPromise() {
-   return new Promise((resolve, reject) => {
-       const a = Math.random();
-       resolve(a);
-   })
-}
-
-
-app.get('/promise', (req, res) => {
-   let results = [];
-   getPromise()
-   .then((result) => {
-       results.push(result)
-   })
-   .then(getPromise)
-   .then((result) => {
-       results.push(result)
-   }).then(() => {
-       res.json({success:true, results: results})
-   })
-})
+app.get('/call/:status', async (req, res) => {
+    const status = req.params.status;
+    let id = await getRandomIdAsPromise();
+    res.json({'success': true, id: id, status: status})
+ })
+ 
+ function getRandomIdAsPromise() {
+    return new Promise((resolve, reject) => {
+        getRandomId((randomId) => {
+            resolve(randomId)
+        })
+    })
+ }
+ 
+ 
+ 
+ function getRandomId(callback) {
+    setTimeout(() => {
+        var random = Math.round(Math.random()*10000)
+        callback(random);
+    }, Math.round(Math.random()*1000))
+ }
+ 
